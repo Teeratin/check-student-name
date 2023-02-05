23032478f21c9a13ce2c595b9c2c9b8463e06dd8
Teeratin <70687065+Teeratin@users.noreply.github.com>
2023-02-06 02:49:29 +0700

    6/2/2023
    successfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,19 +9,18 @@ class AuthController extends Controller
 
     protected $rules = [
         'email' => 'nullable',
-        'password' => 'nullable'
+        'password' => 'nullable',
     ];
 
     protected $messages = [
         'email' => 'The :attribute must be a valid email address.',
-        'required' => 'The :attribute field is required.'
+        'required' => 'The :attribute field is required.',
     ];
 
     protected $attributes = [
         'email' => 'อีเมล',
-        'password' => 'รหัสผ่าน'
+        'password' => 'รหัสผ่าน',
     ];
-
 
     public function login()
     {
@@ -34,7 +33,12 @@ class AuthController extends Controller
         $request->validate($this->rules, $this->messages, $this->attributes);
 
         if (auth()->attempt(['lecturer_username' => $request->input('email'), 'password' => $request->input('password')])) {
-            return redirect()->route('menu');
+            if (auth()->user()->lecturer_type == 1) {
+                return redirect()->route('manage_lecturer_index');
+            } else {
+                return redirect()->route('menu');
+            }
+
         } else {
 
             $error = ['error' => 'อีเมลหรือรหัสผ่านไม่ถูกต้อง'];
